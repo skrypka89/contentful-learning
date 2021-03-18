16660e980255cd19f49c3de35230e5a6985e5a4b
Oleksandr Skrypka <skrypka.o.v@gmail.com>
2021-03-18 23:04:11 +0200

    Added a deleteJsons function

--- getContentful.js
+++ getContentful.js
@@ -8,21 +8,31 @@ Util.inspect.defaultOptions.colors = true;
 const spaceId = '05nc6mbisqy8';
 const accessToken = 'AA7ePv3v7sxuV3NhDDMxkPXOifZ63BHoKd3WSp9jYN8';
 
+const pathResolve = p => path.resolve(__dirname, p);
+const deleteJsons = p => {
+    const files = fs.readdirSync(pathResolve(p));
+    const filenames = files.map(file => {
+        const parts = file.split('.');
+
+        if (/\.json/.test(file) && parts[parts.length - 1] === 'json') {
+            return file;
+        }
+    })
+    filenames.forEach(filename => filename && fs.unlinkSync(pathResolve(p + '/' + filename)));
+};
+const createJsons = entry =>
+    entry.items.forEach(item => {
+        const json = JSON.stringify(item);
+        fs.writeFileSync(pathResolve(`json/${item.fields.name}.json`), json);
+    })
+;
+
 const client = contentful.createClient({
     space: spaceId,
     accessToken: accessToken
-})
-
-client.getEntries({ content_type: 'person'})
-.then(function (entry) {
-  entry.items.forEach(item => createJson(item));
 });
-
-function pathResolve(p) {
-    return path.resolve(__dirname, p);
-}
-
-function createJson(item) {
-    const json = JSON.stringify(item);
-    fs.writeFileSync(`json/${item.fields.name}.json`, json);
-}
+client.getEntries({ content_type: 'person' })
+.then(entry => {
+    deleteJsons('json');
+    createJsons(entry);
+});
